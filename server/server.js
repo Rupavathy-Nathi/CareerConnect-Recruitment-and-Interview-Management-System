56ca9b357e859ad06f47b5bc09ec0fb6d3acfdb5
Rupavathy-Nathi <rupavathyraj@gmail.com>
2025-12-09 18:08:10 +0530

    Fix CORS for Vercel frontend

--- server/server.js
+++ server/server.js
@@ -12,10 +12,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://careerconnect.vercel.app"
-    ],
+    origin: "https://career-connect-psi-bay.vercel.app",
+
     credentials: true
 }));
 app.use(express.json());
